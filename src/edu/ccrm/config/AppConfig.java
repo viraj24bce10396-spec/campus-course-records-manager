85241ac258c1a8911fd16a17fde92a8acdd3315a
viraj24bce10396-spec <viraj.24bce10396@vitbhopal.ac.in>
2025-09-25 22:15:39 +0530

    Update AppConfig.java

--- src/edu/ccrm/config/AppConfig.java
+++ src/edu/ccrm/config/AppConfig.java
@@ -1,1 +1,22 @@
+package edu.ccrm.config;
 
+// Singleton Pattern for config
+public class AppConfig {
+    private static AppConfig instance;
+    private String dataFolder;
+
+    private AppConfig() {
+        dataFolder = "./data";
+    }
+
+    public static AppConfig getInstance() {
+        if (instance == null) {
+            instance = new AppConfig();
+        }
+        return instance;
+    }
+
+    public String getDataFolder() {
+        return dataFolder;
+    }
+}
